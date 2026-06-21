56a10c735cc2742291101fe6168339e562957c40
paulowelton <paulinho.welton08@gmail.com>
2026-06-20 22:59:28 -0300

    spring boot initial architecture

---
+++ backend/src/main/java/com/paulo/beauty_scheduler/BeautySchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.paulo.beauty_scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautySchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeautySchedulerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/paulo/beauty_scheduler/BeautySchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paulo.beauty_scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeautySchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
